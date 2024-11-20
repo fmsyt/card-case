eb3322f916b8fd8288056b1a809e4eb760d40bdb
motsuni <motsuni@yotta-rc.dev>
2024-11-20 12:57:35 +0900

    ページのコンテナを追加し、スタイルを改善

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ const Case = dynamic(() => import("./components/Case"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="h-[100svh] bg-slate-500 grid grid-rows-1fr items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      <main className="bg-slate-200 flex flex-col gap-8 items-center sm:items-start">
-        <Case />
-      </main>
+    <div className="container">
+      <div className="h-[100svh] bg-slate-500 grid grid-rows-1fr items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+        <main className="bg-slate-200 flex flex-col gap-8 items-center sm:items-start">
+          <Case />
+        </main>
+      </div>
     </div>
   );
 }
