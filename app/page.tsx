2671e2b82fbb2160d270a70cf08b7f29abd3795c
motsuni <motsuni@yotta-rc.dev>
2024-11-20 12:56:01 +0900

    ページのスタイルを改善し、コンテナの高さを100svhに設定。グリッドレイアウトを調整し、要素の配置を最適化。

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ const Case = dynamic(() => import("./components/Case"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <div className="bg-slate-500 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="bg-slate-200 flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Case />
-        </main>
-      </div>
+    <div className="h-[100svh] bg-slate-500 grid grid-rows-1fr items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <main className="bg-slate-200 flex flex-col gap-8 items-center sm:items-start">
+        <Case />
+      </main>
     </div>
   );
 }
